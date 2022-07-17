756cd06105809e1cb2f017243bf92ff2cf82332c
jgault87 <99287734+jgault87@users.noreply.github.com>
2022-07-17 13:25:02 -0600

    updated server.js for graphQL

--- server/server.js
+++ server/server.js
@@ -18,14 +18,32 @@ const {authMiddleware} = require('./utils/auth');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+//apply apollo server to express
+server.applyMiddleware({ app });
+
 //middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
+//get all
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+
 
 // app.use(routes);
 
